8eecc04e99054e5dda2acd0365627f8033e4701d
Rayhan Ahmed Imran <114678962+ai-rayhan@users.noreply.github.com>
2023-10-13 23:16:54 +0600

    Module 10 live test

--- lib/main.dart
+++ lib/main.dart
@@ -9,296 +9,84 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Flutter App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      home: SelectionScreen(),
     );
   }
 }
 
-class ShoppingItem {
-  final String name;
-  final String color;
-  final String size;
-  final double unitPrice;
-  int quantity;
-  final String imageUrl;
-
-  ShoppingItem({
-    required this.name,
-    required this.color,
-    required this.size,
-    required this.unitPrice,
-    required this.imageUrl,
-    this.quantity = 0,
-  });
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class SelectionScreen extends StatefulWidget {
+  const SelectionScreen({super.key});
 
   @override
-  _MyHomePage createState() => _MyHomePage();
+  _SelectionScreenState createState() => _SelectionScreenState();
 }
 
-class _MyHomePage extends State<MyHomePage> {
-  List<ShoppingItem> shoppingCart = [
-    ShoppingItem(
-      name: 'Pullover',
-      color: 'Black',
-      size: 'L',
-      unitPrice: 51.0,
-      imageUrl:
-          'https://rukminim2.flixcart.com/image/850/1000/xif0q/t-shirt/l/g/q/xl-ort315-ospo-sports-original-imagghzuryaaazbf.jpeg?q=20',
-    ),
-    ShoppingItem(
-      name: 'T-Shirt',
-      color: 'Gray',
-      size: 'L',
-      unitPrice: 30.0,
-      imageUrl:
-          'https://rukminim2.flixcart.com/image/850/1000/xif0q/shirt/h/8/a/40-asaesh1422-arrow-sports-original-imagskvmjhshy4g3.jpeg?q=90',
-    ),
-    ShoppingItem(
-      name: 'Sport Dress',
-      color: 'Black',
-      size: 'M',
-      unitPrice: 43.0,
-      imageUrl:
-          'https://rukminim2.flixcart.com/image/850/1000/xif0q/t-shirt/u/6/h/xl-ort315-ospo-sports-original-imagghzuvdckdw6p.jpeg?q=20',
-    ),
+class _SelectionScreenState extends State<SelectionScreen> {
+  List<Item> items = [
+    Item("Item 1", false),
+    Item("Item 2", false),
+    Item("Item 3", false),
+    Item("Item 4", false),
+    Item("Item 5", false),
   ];
 
-  void updateTotalAmount() {
-    double total = 0.0;
-    for (var item in shoppingCart) {
-      total += item.unitPrice * item.quantity;
-    }
-    setState(() {
-      totalAmount = total;
-    });
-  }
-
-  double totalAmount = 0.0;
-
-  void showCongratulatorySnackbar() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Congratulations! Your order has been placed.'),
-      ),
-    );
-  }
+  int selectedCount = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white60,
-        elevation: 0,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.search,
-              color: Colors.black,
-            ),
-          )
-        ],
+        title: const Text("Selection Screen"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'My Bag',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: ListView.builder(
-              itemCount: shoppingCart.length,
-              itemBuilder: (context, index) {
-                return ShoppingCartItem(
-                  item: shoppingCart[index],
-                  onQuantityChanged: () {
-                    setState(() {
-                      updateTotalAmount();
-                    });
-                  },
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text('Total amount: '),
-                    Text('${totalAmount.toStringAsFixed(2)}\$'),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Container(
-                  width: double.infinity,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                    ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index].name),
+            tileColor: items[index].selected ? Colors.blue : null,
+            onTap: () {
+              setState(() {
+                items[index].selected = !items[index].selected;
+                if (items[index].selected) {
+                  selectedCount++;
+                } else {
+                  selectedCount--;
+                }
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Selected Items"),
+                content: Text("Number of selected items: $selectedCount"),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text("OK"),
                     onPressed: () {
-                      showCongratulatorySnackbar();
+                      Navigator.of(context).pop();
                     },
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: const Text('CHECK OUT'),
-                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ],
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.check),
       ),
     );
   }
 }
 
-class ShoppingCartItem extends StatefulWidget {
-  final ShoppingItem item;
-  final Function() onQuantityChanged;
+class Item {
+  String name;
+  bool selected;
 
-  ShoppingCartItem({
-    super.key,
-    required this.item,
-    required this.onQuantityChanged,
-  });
-
-  @override
-  _ShoppingCartItemState createState() => _ShoppingCartItemState();
-}
-
-class _ShoppingCartItemState extends State<ShoppingCartItem> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Card(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Container(
-                  color: Colors.black,
-                  width: double.infinity,
-                  height: 150,
-                  child: Image.network(
-                    widget.item.imageUrl,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  width: double.infinity,
-                  height: 150,
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ListTile(
-                          title: Text(
-                            widget.item.name,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                          trailing: const Icon(Icons.more_vert),
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                              'Color: ${widget.item.color}   Size: ${widget.item.size}'),
-                        ),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Card(
-                                elevation: 10,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                child: IconButton(
-                                  onPressed: () {
-                                    if (widget.item.quantity > 0) {
-                                      setState(() {
-                                        widget.item.quantity--;
-                                        widget.onQuantityChanged();
-                                      });
-                                    }
-                                  },
-                                  icon: const Icon(Icons.remove),
-                                ),
-                              ),
-                            ),
-                            Text('${widget.item.quantity}'),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Card(
-                                elevation: 10,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100),
-                                ),
-                                child: IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      widget.item.quantity++;
-                                      widget.onQuantityChanged();
-                                    });
-                                  },
-                                  icon: const Icon(Icons.add),
-                                ),
-                              ),
-                            ),
-                            const Spacer(),
-                            Padding(
-                              padding: const EdgeInsets.only(right: 20),
-                              child: Text("${widget.item.unitPrice}\$"),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  Item(this.name, this.selected);
 }
